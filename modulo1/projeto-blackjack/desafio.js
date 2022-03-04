17d2e1f94a972b4484e665a7e32a152acfcfe2c4
Bryan Fernandes <bryanfernandes8@gmail.com>
2022-03-03 22:17:12 -0300

    melhorei e simplifiquei o código

--- modulo1/projeto-blackjack/desafio.js
+++ modulo1/projeto-blackjack/desafio.js
@@ -11,137 +11,88 @@
  * 
  */
 
-function comecaJogo(){
-   for (let i=0 ; i<2; i++){
-
-      jogadorUsuario.baralho.push(comprarCarta())
-      jogadorMaquina.baralho.push(comprarCarta())
-
-   }
-}
-
-function testaCartasIniciais(array){
-   let contador = 0
-   array.forEach((element)=> {
-       if(element.includes("A")){
-           contador++
-       }
-   })
-   if(contador === 2){
-       return true
-   }else{
-       return false
-   }
-}
-
-function retribuirCartas(){
-   jogadorUsuario.cartas = [jogadorUsuario.baralho[0].texto , jogadorUsuario.baralho[1].texto]
-   jogadorMaquina.cartas = [jogadorMaquina.baralho[0].texto , jogadorMaquina.baralho[1].texto]
-}
-
-function verificaCartasIniciais(array, array2){
-
-   
-   let teste = testaCartasIniciais(array)
-   let teste2 = testaCartasIniciais(array2)
-   
-   
-   while(teste || teste2){
-      jogadorUsuario.baralho.splice(0,2)
-      jogadorMaquina.baralho.splice(0,2)
-   
-      comecaJogo()
-      retribuirCartas()
-      
-      teste = testaCartasIniciais(jogadorUsuario.cartas)
-      teste2 = testaCartasIniciais(jogadorMaquina.cartas)
-}
-   
-}
-
-function compraCartaESomaPontuacao(contador, jogador){
-   let i = contador
-
-   jogador.baralho.push(comprarCarta())
-   jogador.cartas.push(jogador.baralho[i].texto)
-      
-   jogador.pontuacao+= jogador.baralho[i].valor
-}
-
-function iniciaRodadaUsuario(){
-   let contador = 2
-
-   while(jogadorUsuario.pontuacao <= 21 && confirm(`Suas cartas são "${jogadorUsuario.cartas}". A carta revelada do computador é "${jogadorMaquina.cartas[0]}"`  +
-   "\n"+
-   "Deseja comprar mais uma carta?")){
-      compraCartaESomaPontuacao(contador, jogadorUsuario)
-      contador++
-   }
-}
-function iniciaRodadaMaquina(){
-   let contador = 2
-   if(jogadorUsuario.pontuacao <=21){
-
-      while(jogadorMaquina.pontuacao < 21 && jogadorMaquina.pontuacao < jogadorUsuario.pontuacao){
-         compraCartaESomaPontuacao(contador, jogadorMaquina)
-         contador++
-      }
-   }
-}
-
-function mostraResultado(){
-
-   const empate = jogadorUsuario.pontuacao === jogadorMaquina.pontuacao
-   const perdeu = jogadorUsuario.pontuacao < jogadorMaquina.pontuacao || jogadorUsuario.pontuacao > 21
-   const ganhou = jogadorUsuario.pontuacao > jogadorMaquina.pontuacao || jogadorMaquina.pontuacao > 21
-   
-   
-   if(empate){
-      alert(`Suas cartas são "${jogadorUsuario.cartas}" . Sua pontuação é "${jogadorUsuario.pontuacao}".\n` +
-      `As cartas do computador são "${jogadorMaquina.cartas}" . A pontuação do computador é "${jogadorMaquina.pontuacao}".\n` +
-      "Empate!")
-      }
-   else if(perdeu){
-      alert(`Suas cartas são "${jogadorUsuario.cartas}" . Sua pontuação é "${jogadorUsuario.pontuacao}".\n` +
-      `As cartas do computador são "${jogadorMaquina.cartas}" . A pontuação do computador é "${jogadorMaquina.pontuacao}".\n` +
-      "O computador ganhou!")
-      }
-   else if(ganhou){
-      alert(`Suas cartas são "${jogadorUsuario.cartas}" . Sua pontuação é "${jogadorUsuario.pontuacao}".\n` +
-      `As cartas do computador são "${jogadorMaquina.cartas}" . A pontuação do computador é "${jogadorMaquina.pontuacao}".\n` +
-      "O usuário ganhou!")
-      }
-}
-
-
-const jogadorUsuario = {
-      baralho: [],
-      pntuacao: null
-   }
-
-const jogadorMaquina = {
-      baralho: [],
-      pontuacao:null
-   }
-     
-
-
-alert("Boas vindas ao jogo de blackjack!")
-
-if(confirm("Quer iniciar uma nova rodada?")){
-   
-   comecaJogo()
-   retribuirCartas()
-   verificaCartasIniciais(jogadorUsuario.cartas, jogadorMaquina.cartas)
-   
-   jogadorUsuario.pontuacao = jogadorUsuario.baralho[0].valor + jogadorUsuario.baralho[1].valor
-   jogadorMaquina.pontuacao = jogadorMaquina.baralho[0].valor + jogadorMaquina.baralho[1].valor
-   
-   iniciaRodadaUsuario()
-   iniciaRodadaMaquina()
-      
-   mostraResultado()
-   
-   }else{
-      alert("O jogo acabou!")
-   }
+    let jogadorUsuario = []
+    let jogadorComputador = []
+    
+    //Jogo rodando
+    alert("Boas vindas ao jogo de Blackjack")
+    if(confirm("deseja iniciar uma nova rodada ?")){
+    iniciarJogo()
+    IniciarRodada()
+    mostraResultado()
+    }else{
+       alert("O jogo acabou!")
+    }
+    
+    //Inicia o jogo e verifica cartas iniciais
+    function iniciarJogo(){
+        
+        let teste = true
+        
+        while(teste){
+            for(let i=0; i<2 ; i++){
+            jogadorUsuario.push(comprarCarta())
+            jogadorComputador.push(comprarCarta())
+            }
+            
+            teste =  ((jogadorComputador[0].texto.includes("A") && jogadorComputador[1].texto.includes("A")) || (jogadorUsuario[0].texto.includes("A") && jogadorUsuario[1].texto.includes("A")))
+             if(teste){
+                 jogadorUsuario = []
+                 jogadorComputador = []
+             }
+        }
+    }
+    
+    //Inicia a rodada do usuário
+    function mostrarCartas(jogador){
+        const cartas = []
+        for(let i =0; i < jogador.length; i++){
+            cartas.push(`${jogador[i].texto} `)
+        }
+        return cartas
+    }
+    function contaPontuacao(jogador){
+        let pontuacao = 0;
+        for(let i =0; i < jogador.length; i++){
+           pontuacao += jogador[i].valor 
+        }
+        return pontuacao
+    }
+    
+    function IniciarRodada(){
+        
+        while( contaPontuacao(jogadorUsuario) <= 21 && confirm(`Suas cartas são ${mostrarCartas(jogadorUsuario)}. A cartas revelada do computador é ${jogadorComputador[0].texto} \n deseja comprar mais uma carta ?`) ){
+            jogadorUsuario.push(comprarCarta()) 
+        } 
+    
+    //Inicia a rodada do computador
+        if(contaPontuacao(jogadorUsuario) <= 21){
+    
+            while(contaPontuacao(jogadorComputador) < contaPontuacao(jogadorUsuario) && contaPontuacao(jogadorComputador) < 21){
+                jogadorComputador.push(comprarCarta())
+            }
+        }
+    }
+    
+    //mostra o resultado
+    function mostraResultado(){
+        const ganhou = (contaPontuacao(jogadorComputador) < contaPontuacao(jogadorUsuario) || contaPontuacao(jogadorComputador) > 21) && contaPontuacao(jogadorUsuario) <= 21
+        const perdeu = (contaPontuacao(jogadorComputador) > contaPontuacao(jogadorUsuario) || contaPontuacao(jogadorUsuario) > 21) && contaPontuacao(jogadorComputador) <= 21
+        const empate = contaPontuacao(jogadorComputador) === contaPontuacao(jogadorUsuario)
+    
+        if(empate){
+            alert(`Suas cartas são "${mostrarCartas(jogadorUsuario)}" . Sua pontuação é "${contaPontuacao(jogadorUsuario)}".\n` +
+            `As cartas do computador são "${mostrarCartas(jogadorComputador)}" . A pontuação do computador é "${contaPontuacao(jogadorComputador)}".\n` +
+            "Empate!")
+            }
+         else if(perdeu){
+            alert(`Suas cartas são "${mostrarCartas(jogadorUsuario)}" . Sua pontuação é "${contaPontuacao(jogadorUsuario)}".\n` +
+            `As cartas do computador são "${mostrarCartas(jogadorComputador)}" . A pontuação do computador é "${contaPontuacao(jogadorComputador)}".\n` +
+            "O computador ganhou!")
+            }
+         else if(ganhou){
+            alert(`Suas cartas são "${mostrarCartas(jogadorUsuario)}" . Sua pontuação é "${contaPontuacao(jogadorUsuario)}".\n` +
+            `As cartas do computador são "${mostrarCartas(jogadorComputador)}" . A pontuação do computador é "${contaPontuacao(jogadorComputador)}".\n` +
+            "O usuário ganhou!")
+            }
+    }
--- modulo1/projeto-blackjack/testes.js
+++ modulo1/projeto-blackjack/testes.js
@@ -1,5 +1,81 @@
-const array2 = ["A", "A"]
+let jogadorUsuario = []
+let jogadorComputador = []
+
+//Jogo rodando
+iniciarJogo()
+IniciarRodada()
+mostraResultado()
 
 
+//Inicia o jogo e verifica cartas iniciais
+function iniciarJogo(){
+    
+    let teste = true
+    
+    while(teste){
+        for(let i=0; i<2 ; i++){
+        jogadorUsuario.push(comprarCarta())
+        jogadorComputador.push(comprarCarta())
+        }
+        
+        teste =  ((jogadorComputador[0].texto.includes("A") && jogadorComputador[1].texto.includes("A")) || (jogadorUsuario[0].texto.includes("A") && jogadorUsuario[1].texto.includes("A")))
+         if(teste){
+             jogadorUsuario = []
+             jogadorComputador = []
+         }
+    }
+}
 
- console.log(testeArray(array2))
+//Inicia a rodada do usuário
+function mostrarCartas(jogador){
+    const cartas = []
+    for(let i =0; i < jogador.length; i++){
+        cartas.push(`${jogador[i].texto} `)
+    }
+    return cartas
+}
+function contaPontuacao(jogador){
+    let pontuacao = 0;
+    for(let i =0; i < jogador.length; i++){
+       pontuacao += jogador[i].valor 
+    }
+    return pontuacao
+}
+
+function IniciarRodada(){
+    
+    while( contaPontuacao(jogadorUsuario) <= 21 && confirm(`Suas cartas são ${mostrarCartas(jogadorUsuario)}. A cartas revelada do computador é ${jogadorComputador[0].texto} \n deseja comprar mais uma carta ?`) ){
+        jogadorUsuario.push(comprarCarta()) 
+    } 
+
+//Inicia a rodada do computador
+    if(contaPontuacao(jogadorUsuario) <= 21){
+
+        while(contaPontuacao(jogadorComputador) < contaPontuacao(jogadorUsuario) && contaPontuacao(jogadorComputador) < 21){
+            jogadorComputador.push(comprarCarta())
+        }
+    }
+}
+
+//mostra o resultado
+function mostraResultado(){
+    const ganhou = (contaPontuacao(jogadorComputador) < contaPontuacao(jogadorUsuario) || contaPontuacao(jogadorComputador) > 21) && contaPontuacao(jogadorUsuario) <= 21
+    const perdeu = (contaPontuacao(jogadorComputador) > contaPontuacao(jogadorUsuario) || contaPontuacao(jogadorUsuario) > 21) && contaPontuacao(jogadorComputador) <= 21
+    const empate = contaPontuacao(jogadorComputador) === contaPontuacao(jogadorUsuario)
+
+    if(empate){
+        alert(`Suas cartas são "${mostrarCartas(jogadorUsuario)}" . Sua pontuação é "${contaPontuacao(jogadorUsuario)}".\n` +
+        `As cartas do computador são "${mostrarCartas(jogadorComputador)}" . A pontuação do computador é "${contaPontuacao(jogadorComputador)}".\n` +
+        "Empate!")
+        }
+     else if(perdeu){
+        alert(`Suas cartas são "${mostrarCartas(jogadorUsuario)}" . Sua pontuação é "${contaPontuacao(jogadorUsuario)}".\n` +
+        `As cartas do computador são "${mostrarCartas(jogadorComputador)}" . A pontuação do computador é "${contaPontuacao(jogadorComputador)}".\n` +
+        "O computador ganhou!")
+        }
+     else if(ganhou){
+        alert(`Suas cartas são "${mostrarCartas(jogadorUsuario)}" . Sua pontuação é "${contaPontuacao(jogadorUsuario)}".\n` +
+        `As cartas do computador são "${mostrarCartas(jogadorComputador)}" . A pontuação do computador é "${contaPontuacao(jogadorComputador)}".\n` +
+        "O usuário ganhou!")
+        }
+}
