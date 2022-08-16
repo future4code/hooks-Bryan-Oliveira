b4c8e7d795204cdc10bba56755469b95ea7a8f96
Helany Melo <76518732+helanymelo@users.noreply.github.com>
2022-08-16 14:17:01 -0300

    Update ex2.js

--- modulo5/lista-typescript/src/ex2/ex2.js
+++ modulo5/lista-typescript/src/ex2/ex2.js
@@ -1,12 +1,10 @@
-"use strict";
 // exercicio 2:
-const toType = function (obj) {
+var toType = function (obj) {
     // @ts-ignore
     return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
 };
-const whatTypeIs = (param) => {
+var whatTypeIs = function (param) {
     console.log(toType(param));
 };
 // console.log(toType([1,2,3]))
 module.exports = whatTypeIs;
-//# sourceMappingURL=ex2.js.map
