b4f87eb460c4c42792826a48f6dc071307467e5a
Bryan Fernandes <bryanfernandes8@gmail.com>
2022-08-24 13:41:52 -0300

    exercicios

---
+++ modulo5/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,7 @@
+import { Product } from "./types";
+
+export const products: Product[] = [
+    {id: "cfa07cb4-5979-46b2-adfc-80418e50220e", name: "uva", price: 3},
+    {id: "cfa07cb4-5979-46b2-adec-80418e50227e", name: "maca", price: 5},
+    {id: "cfh97cb4-5979-46b2-adfc-80418e50220e", name: "pera", price: 7},
+]
